e0c7495ba484caa586e87bb9a4679f665f10ef38
Haltoen <hjaltipp@hotmail.com>
2024-01-28 02:54:19 +0100

    added simple ui

--- src/Plots.py
+++ src/Plots.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import mplfinance as mpf 
+import os
+import ctypes
 
 import ApiHandler as api 
 
@@ -9,28 +11,37 @@ colors = mpf.make_marketcolors(up="#00ff00",
                                edge="inherit",
                                volume="in")
 
-cutom_style = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=colors)
+custom_style = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=colors)
 
-def make_plot (data, show_volume, logarithmic): 
-    fig, axlist = mpf.plot(data ,type="candle", style = cutom_style, volume=show_volume, returnfig=True)
+data = api.data_request("BTC-USD", 20, "1wk")
+
+width = 1920  # Replace with your desired width
+height = 1080  # Replace with your desired height
+
+def plot_as_background(data, custom_style, show_volume, logarithmic, width, height):
+    # Create a new figure with the specified width and height
+    fig, axlist = mpf.plot(data, type="candle", style=custom_style, volume=show_volume, returnfig=True)
+
+    # Set the facecolor of the figure to be white (non-transparent)
+    fig.patch.set_facecolor('white')
+
+    # Set the DPI based on the specified width and height
+    dpi_value = int(width / fig.get_size_inches()[0])
+    fig.set_dpi(dpi_value)
+
+        # Set logarithmic scaling for the y-axis if requested
     if logarithmic:
         ax = axlist[0]
         ax.set_yscale('log')
+
+    # Save the figure as an image file
+    background_image_filename = 'background_image.png'
+    fig.savefig(background_image_filename, dpi=dpi_value)
+
+    ctypes.windll.user32.SystemParametersInfoW(20,0,os.path.abspath(background_image_filename),0)
+
     return fig
 
 
-data = api.data_request("BTC", 20, "1wk")
-
-fig = make_plot(data, False, True)
-
-# Set the background image filename (adjust the filename and path as needed)
-background_image_filename = 'background_image.png'
-
-# Save the figure as an image file
-fig.patch.set_facecolor('white')
-fig.savefig(background_image_filename)  
-
-
-import ctypes
-SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, background_image_filename, 3)
+def simple_plot (data, show_volume, logarithmic):
+    plot_as_background(data, custom_style, show_volume, logarithmic, width, height)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,97 @@
 ## application layer
+
+import customtkinter as ctk
+import tkinter as tk
+import ApiHandler as api
+import Plots as my_plt
+
+
+
+ 
+# Basic parameters and initializations
+# Supported modes : Light, Dark, System
+ctk.set_appearance_mode("System") 
+ 
+# Supported themes : green, dark-blue, blue
+ctk.set_default_color_theme("green")    
+ 
+appWidth, appHeight = 600, 500
+ 
+# App Class
+class App(ctk.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+ 
+        self.title("Stonkpaper Engine")
+        self.geometry(f"{appWidth}x{appHeight}")
+ 
+        # Ticker Label
+        self.tickerLabel = ctk.CTkLabel(self,
+                                      text="Ticker")
+        self.tickerLabel.grid(row=0, column=0,
+                            padx=20, pady=20,
+                            sticky="ew")
+ 
+        # Ticker Entry Field
+        self.tickerEntry = ctk.CTkEntry(self,
+                         placeholder_text="Teja")
+        self.tickerEntry.grid(row=0, column=1,
+                            columnspan=3, padx=20,
+                            pady=20, sticky="ew")
+ 
+        # Years Label
+        self.timeLabel = ctk.CTkLabel(self,
+                                     text="years")
+        self.timeLabel.grid(row=1, column=0,
+                           padx=20, pady=20,
+                           sticky="ew")
+ 
+        # Years Entry Field
+        self.timeEntry = ctk.CTkEntry(self,
+                            placeholder_text="18")
+        self.timeEntry.grid(row=1, column=1,
+                           columnspan=3, padx=20,
+                           pady=20, sticky="ew")
+
+ 
+        # Choice Label
+        self.choiceLabel = ctk.CTkLabel(self,
+                                        text="Options")
+        self.choiceLabel.grid(row=2, column=0,
+                              padx=20, pady=20,
+                              sticky="ew")
+        self.choice1 = ctk.CTkCheckBox(self, text="Show Volume")
+        self.choice1.grid(row=2, column=1, padx=20,
+                          pady=20, sticky="ew")
+ 
+        self.choice2 = ctk.CTkCheckBox(self, text="Log y-axis")                               
+        self.choice2.grid(row=2, column=2, padx=20, pady=20,
+                          sticky="ew")
+ 
+        # Generate Button
+        self.generateResultsButton = ctk.CTkButton(self,
+                                         text="Generate Plot",
+                                         command=self.button_clicked)
+        self.generateResultsButton.grid(row=3, column=1,
+                                        columnspan=2,
+                                        padx=20, pady=20,
+                                        sticky="ew")
+        
+    def button_clicked(self):
+        ticker = self.tickerEntry.get()
+        time = int(self.timeEntry.get())
+        volume = self.choice1._check_state
+        log = self.choice1._check_state
+        data = api.data_request(ticker, time, "1wk")
+        plot = my_plt.simple_plot(data, volume, log)
+        return None
+
+
+        
+
+
+    #def scedule_plot
+ 
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
