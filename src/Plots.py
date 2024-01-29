f54596ae7caeb658e94facbdc56871a66397518e
Haltoen <hjaltipp@hotmail.com>
2024-01-30 00:50:20 +0100

    works

--- src/Plots.py
+++ src/Plots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import mplfinance as mpf 
 import os
 import ctypes
+from screeninfo import get_monitors
 
 import ApiHandler as api 
 
@@ -15,19 +16,28 @@ custom_style = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=col
 
 data = api.data_request("BTC-USD", 20, "1wk")
 
-width = 1920  # Replace with your desired width
-height = 1080  # Replace with your desired height
+# Get information about the primary monitor
+primary_monitor = get_monitors()[0]
 
-def plot_as_background(data, custom_style, show_volume, logarithmic, width, height):
+# Retrieve width and height of the primary monitor
+monitor_width = primary_monitor.width
+monitor_height = primary_monitor.height
+dpi_value = 96
+figsize = (monitor_width / dpi_value, monitor_height / dpi_value)
+
+
+
+def plot_as_background(data, custom_style, show_volume, logarithmic):
     # Create a new figure with the specified width and height
-    fig, axlist = mpf.plot(data, type="candle", style=custom_style, volume=show_volume, returnfig=True)
+    fig, axlist = mpf.plot(data, type="candle", style=custom_style, volume=show_volume, returnfig=True, figsize=figsize)
 
     # Set the facecolor of the figure to be white (non-transparent)
     fig.patch.set_facecolor('white')
 
     # Set the DPI based on the specified width and height
-    dpi_value = int(width / fig.get_size_inches()[0])
     fig.set_dpi(dpi_value)
+
+    
 
         # Set logarithmic scaling for the y-axis if requested
     if logarithmic:
@@ -44,4 +54,4 @@ def plot_as_background(data, custom_style, show_volume, logarithmic, width, heig
 
 
 def simple_plot (data, show_volume, logarithmic):
-    plot_as_background(data, custom_style, show_volume, logarithmic, width, height)
+    plot_as_background(data, custom_style, show_volume, logarithmic)
--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ import customtkinter as ctk
 import tkinter as tk
 import ApiHandler as api
 import Plots as my_plt
-
-
+import os 
+import time
+import multiprocessing
+from datetime import datetime
 
  
 # Basic parameters and initializations
@@ -16,7 +18,43 @@ ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("green")    
  
 appWidth, appHeight = 600, 500
- 
+
+
+# File to store the last execution time
+state_file = 'last_execution.txt'
+
+def read_last_execution_time():
+    if os.path.exists(state_file):
+        with open(state_file, 'r') as file:
+            return float(file.read().strip())
+    else:
+        return 0.0
+    
+
+def write_last_execution_time(current_time):
+    with open(state_file, 'w') as file:
+        file.write(str(current_time))
+
+
+
+def schedule_plots(ticker, volume, log, data):
+    interval_minutes = 30  # Change this to your desired interval in minutes
+    interval_seconds = interval_minutes * 60
+    first = True
+    while True:
+        last_execution_time = read_last_execution_time()
+        current_time = time.time()
+
+        if current_time - last_execution_time >= interval_seconds or first :
+            # update plot
+            print("attempt at data retrival")
+            my_plt.simple_plot(data, volume, log)
+            # Update last execution time
+            write_last_execution_time(current_time)
+
+        time.sleep(60)  # Sleep for 1 minute 
+
+
 # App Class
 class App(ctk.CTk):
     def __init__(self, *args, **kwargs):
@@ -70,7 +108,7 @@ class App(ctk.CTk):
  
         # Generate Button
         self.generateResultsButton = ctk.CTkButton(self,
-                                         text="Generate Plot",
+                                         text="Set as Background",
                                          command=self.button_clicked)
         self.generateResultsButton.grid(row=3, column=1,
                                         columnspan=2,
@@ -78,20 +116,29 @@ class App(ctk.CTk):
                                         sticky="ew")
         
     def button_clicked(self):
+        self.terminate_other_processes()
+
         ticker = self.tickerEntry.get()
-        time = int(self.timeEntry.get())
+        periods = int(self.timeEntry.get())
         volume = self.choice1._check_state
-        log = self.choice1._check_state
-        data = api.data_request(ticker, time, "1wk")
-        plot = my_plt.simple_plot(data, volume, log)
-        return None
+        log = self.choice2._check_state
+        data = api.data_request(ticker, periods, "1wk")
 
-
+        # Create a process for scheduling plots 
+        plot_process = multiprocessing.Process(target=schedule_plots, args=(ticker, volume, log, data))
+        plot_process.start()
         
+        return None
+    
+    def terminate_other_processes(self):
+        current_process = multiprocessing.current_process()
+        for process in multiprocessing.active_children():
+            if process != current_process:
+                process.terminate()
+                process.join()  # Ensure the terminated process is cleaned up
 
 
-    #def scedule_plot
- 
+
 if __name__ == "__main__":
     app = App()
     app.mainloop()
